00206807704958f54d269e8ea15b999c68bd82f9
Instructor <jsitbon@qcc.cuny.edu>
2019-07-05 13:34:11 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class Counter extends Component {
         <div className="navbar">Counter.js</div>
         <div className="counter">
           <h1>What do we put here??? :)</h1>
-          <button type="button">Increment</button>
+          <button type="button">tncrement</button>
           <button type="button">Decrement</button>
         </div>
       </div>
